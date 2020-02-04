d67ea8f99fd94ae8faeceb7c4f96b5aff8c47349
francojmf <francojmf@hotmail.com>
2020-02-03 22:27:00 -0300

    atualizados arquivo config para backend

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "https://spring-ionic-nelio.herokuapp.com",
-    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
+  baseUrl: "https://cria-backend.herokuapp.com",
+  bucketBaseUrl: "https://cria01.s3-sa-east-1.amazonaws.com"
 }
